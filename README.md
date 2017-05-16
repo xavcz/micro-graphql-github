0a17515433a3c08d9d24fd855e290c4772da97a4
xavcz <xavier.cazalot@gmail.com>
2017-05-16 14:06:26 +0200

    handle errors and add github token

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # micro-graphql-github
-:octocat: 
+Used internally for https://xav.cz :octocat: 
+
+> Note: Need a `GITHUB_TOKEN` as an environment variable.
--- src/schema.js
+++ src/schema.js
@@ -30,40 +30,49 @@ const repoNames = [
 const resolvers = {
   Query: {
     repositories: async () => {
-      // fetch repositories data
-      const rawResponses = await Promise.all(
-        repoNames.map(name => fetch(`https://api.github.com/repos/${name}`))
-      );
+      try {
+        // fetch repositories data
+        const rawResponses = await Promise.all(
+          repoNames.map(name =>
+            fetch(`https://api.github.com/repos/${name}?access_token=${process.env.GITHUB_TOKEN}`)
+          )
+        );
 
-      // transform responses in exploitable JSON
-      const repositories = await Promise.all(rawResponses.map(res => res.json()));
+        // transform responses in exploitable JSON
+        const repositories = await Promise.all(rawResponses.map(res => res.json()));
 
-      // // get color palettes for each logo
-      // const rawPalettes = await Promise.all(
-      //   repositories.map(({ owner: { avatar_url } }) =>
-      //     fetch('https://micro-color-palette-lfupybrqsd.now.sh', {
-      //       method: 'POST',
-      //       body: JSON.stringify({ src: avatar_url }),
-      //     })
-      //   )
-      // );
+        // // get color palettes for each logo
+        // const rawPalettes = await Promise.all(
+        //   repositories.map(({ owner: { avatar_url } }) =>
+        //     fetch('https://micro-color-palette-lfupybrqsd.now.sh', {
+        //       method: 'POST',
+        //       body: JSON.stringify({ src: avatar_url }),
+        //     })
+        //   )
+        // );
 
-      // // transform responses in exploitable JSON
-      // const palettes = await Promise.all(rawPalettes.map(res => res.json()));
+        // // transform responses in exploitable JSON
+        // const palettes = await Promise.all(rawPalettes.map(res => res.json()));
 
-      // // extract the primary color
-      // const primaryColors = palettes.map(([primaryColor]) => primaryColor);
+        // // extract the primary color
+        // const primaryColors = palettes.map(([primaryColor]) => primaryColor);
 
-      return repositories.map((repo /*, index */) => ({
-        id: repo.id,
-        name: repo.full_name,
-        description: repo.description,
-        image: repo.owner.avatar_url,
-        // color: primaryColors[index],
-        stars: repo.stargazers_count,
-        forks: repo.forks_count,
-        url: repo.html_url,
-      }));
+        return repositories.map((repo /*, index */) => ({
+          id: repo.id,
+          name: repo.name,
+          description: repo.description,
+          image: repo.name.includes('codetours')
+            ? 'https://www.codetours.xyz/icon.png' // the org logo doesn't render well for this repo
+            : repo.owner.avatar_url,
+          // color: primaryColors[index],
+          stars: repo.stargazers_count,
+          forks: repo.forks_count,
+          url: repo.html_url,
+        }));
+      } catch (e) {
+        console.log(e.stack); // eslint-disable-line
+        throw Error(e);
+      }
     },
   },
 };
